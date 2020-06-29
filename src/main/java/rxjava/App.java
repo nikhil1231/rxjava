2f412df443af694ec42ab3f1c146d889bc7ab358
nikhil1231 <nikhil1231@hotmail.co.uk>
2020-06-29 12:54:54 +0100

    Simple observer/obserable combo

--- src/main/java/rxjava/App.java
+++ src/main/java/rxjava/App.java
@@ -3,9 +3,39 @@
  */
 package rxjava;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Gradle set up and RxJava added to project.");
+        Observable<Integer> observable = Observable.just(1,2,3,4,5);
+
+        Observer<Integer> observer = new Observer<Integer>() {
+
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("Started observing");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                System.out.println(integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("Error: " + e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Completed observing");
+            }
+        };
+
+        observable.subscribe(observer);
     }
 }
